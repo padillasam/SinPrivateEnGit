235b04d82d4de62b5a6c7ec63b165caecd5b72b9
srpadilla <srpadilla@misena.edu.co>
2020-02-18 21:37:26 -0500

    Nueva clase en windows

---
+++ src/introgit/IntroWindows.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package introgit;
+
+/**
+ *
+ * @author Samuel Padilla
+ */
+public class IntroWindows {
+    public static void main(String[] args) {
+        System.out.println("En windows, funciona perfecto");
+    }
+    
+}
