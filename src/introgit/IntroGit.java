bbb7aa43a68a7747b8fde385e39a6a7a889a1e8c
padillasam <srpadilla@misena.edu.co>
2020-02-18 20:46:01 -0500

    Primer commit 18-II-2020 a las 20:46

---
+++ src/introgit/IntroGit.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package introgit;
+
+/**
+ *
+ * @author samuel
+ */
+public class IntroGit {
+    public static void main(String[] args) {
+        System.out.println("Estoy en Linux 18-II-2020 20:20");
+    }
+    
+}
